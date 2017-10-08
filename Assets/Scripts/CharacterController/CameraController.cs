92d222bc48cdc7ded2806cd927c85ad0d99cb89b
MathieuAuclair <mathiass12@hotmail.com>
2017-10-07 23:41:48 -0400

    adding player resources

--- Assets/Scripts/CharacterController/CameraController.cs
+++ Assets/Scripts/CharacterController/CameraController.cs
@@ -2,24 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Analysis disable once CheckNamespace
 public class CameraController : MonoBehaviour {
 
     Vector2 mouseLook, smoothV;
     public float sensitivity = 5.0f, smoothing = 2.0f;
     GameObject character;
-    //// Use this for initialization
-    //public float mouseSensitivity = 100.0f;
-    //public float clampAngle = 80.0f;
-
-    //private float rotY = 0.0f; // rotation around the up/y axis
-    //private float rotX = 0.0f; // rotation around the right/x axis
 
     void Start()
     {
         character = this.transform.parent.gameObject;
-        //Vector3 rot = transform.localRotation.eulerAngles;
-        //rotY = rot.y;
-        //rotX = rot.x;
     }
 
     void Update()
@@ -33,15 +25,5 @@ public class CameraController : MonoBehaviour {
 
         transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
         character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
-        //float mouseX = Input.GetAxis("Mouse X");
-        //float mouseY = -Input.GetAxis("Mouse Y");
-
-        //rotY += mouseX * mouseSensitivity * Time.deltaTime;
-        //rotX += mouseY * mouseSensitivity * Time.deltaTime;
-
-        //rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
-
-        //Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
-        //transform.rotation = localRotation;
     }
 }
