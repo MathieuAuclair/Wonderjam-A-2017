aa45e3aac1bc5600189a1b6e147a0f1cd3dbde44
MathieuAuclair <mathiass12@hotmail.com>
2017-10-08 15:23:23 -0400

    V1.0

---
+++ Assets/EndGameTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameTrigger : MonoBehaviour {
+	void OnTriggerEnter(Collider coll){
+		if (coll.tag == "Player") {
+			Application.LoadLevel ("RenderTexture");
+		} else {
+			Debug.Log (coll.tag);
+		}
+	}
+}
---
+++ Assets/ScenarioOffice.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioOffice : MonoBehaviour {
+
+    GameObject Player;
+
+    void Awake(){
+        Player = Instantiate(Resources.Load("Player"), transform.position, Quaternion.identity) as GameObject;
+    }
+
+    public void EndRound(){
+        DontDestroyOnLoad(this);
+    }
+}
